a64a940136e2612c1b1496a0306c0cb8dab70824
JJ <jj.spetseris@yahoo.com>
2017-11-09 22:49:47 -0600

    initial commit

---
+++ validator/get.js
@@ -0,0 +1,46 @@
+// TODO
+// get error and warnings from https://validator.w3.org/ for HTML validation
+// get error and warnings from https://jigsaw.w3.org/css-validator/ for CSS validation
+// get test speed date from https://developers.google.com/speed/pagespeed/insights/
+// make system for looking deeper based on class and scrapper
+
+
+// HTML validation
+// const validator = require('html-validator')
+// const options = {
+//  url: 'https://jjspetseris.com',
+//  format: 'json'
+// }
+//
+// validator(options)
+//   .then((data) => {
+//     console.log(data.messages.length)
+//   })
+//   .catch((error) => {
+//     console.error(error)
+//   })
+
+// TODO
+// from data pull error & warning count as seperate thing
+
+
+// CSS validation
+// var validator = require('w3c-css');
+//
+// validator.validate('https://jjspetseris.com/', function(err, data) {
+//   if(err) {
+//     // an error happened
+//     console.error(err);
+//   } else {
+//     // validation errors
+//     console.log('validation errors', data.errors.length);
+//
+//     // validation warnings
+//     console.log('validation warnings', data.warnings.length);
+//   }
+// });
+
+// TODO
+// done
+
+// Speed tester
